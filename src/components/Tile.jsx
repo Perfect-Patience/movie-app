d8710e245633346ade871731a605ae93cbfa080c
Perfect Dah <patiencedah@gmail.com>
2025-06-14 23:49:57 +0000

    adds box shadow to tile design

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 function Tile({movie}) {
   return (
     <div className='w-[15rem] '>
-        <div>
+        <div className='relative'>
         <img className="object-cover rounded-2xl" src={`${import.meta.env.VITE_TMDB_API_IMAGE}${
                   movie.poster_path
                 }`} />
+                <div className='absolute bottom-0 left=0 w-full h-1/3' style={{background: 'linear-gradient(to bottom, rgba(0,0,0,0) 0%, #1e2939 100%)'}}> 
+
+                </div>
         </div>
-        <p className='text-white text-xl mt-1'>{movie.title? movie.title : movie.name}</p>
+        <p className='text-white text-xl mt-1' >{movie.title? movie.title : movie.name}</p>
     </div>
   )
 }
